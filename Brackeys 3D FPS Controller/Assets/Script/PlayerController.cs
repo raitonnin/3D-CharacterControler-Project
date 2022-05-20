7c1e30e30e879a5a845cae5172ca9ccd78862ba0
raitonnin <45186265+raitonnin@users.noreply.github.com>
2022-05-20 18:56:43 -0500

    Added Jump and Grav
    Next Build scene in pro builder
    make some sort of infinite runner maybe auto generated world

--- Brackeys 3D FPS Controller/Assets/Script/PlayerController.cs
+++ Brackeys 3D FPS Controller/Assets/Script/PlayerController.cs
@@ -7,14 +7,47 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public float maxFallSpeed = -50f;
+
+    public float minFallSpeed = -2f;
+
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    bool isGrounded;
     // Update is called once per frame
     void Update()
     {
+//GroundCheck
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+//Velocity Sets
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = minFallSpeed;
+        }
+        if (velocity.y < maxFallSpeed)
+            { velocity.y = maxFallSpeed;}
+
+//INPUTS
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+//Walk
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+//Gravity
+        velocity.y += gravity * Time.deltaTime;
+//Move character using its velocity vector 3
+        controller.Move(velocity * Time.deltaTime);
     }
 }
